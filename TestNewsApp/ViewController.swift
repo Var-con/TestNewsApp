53dbb0236fb9fbbe42db6967406d123607dd166c
Stanislav Klimov <kpuctallaz@gmail.com>
2020-09-27 17:48:19 +0500

    Initial Commit

---
+++ TestNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestNewsApp
+//
+//  Created by Станислав Климов on 27.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
